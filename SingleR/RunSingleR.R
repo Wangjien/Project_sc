f0d439d2cb7958699dfebc774bc54c8e29a1f2da
Wangjien <52444578+Wangjien@users.noreply.github.com>
2023-02-08 09:11:48 +0800

    Update RunSingleR.R

--- SingleR/RunSingleR.R
+++ SingleR/RunSingleR.R
@@ -26,3 +26,11 @@ RunSingleR <- function(
     return(celltype)
 }
 RunSingleR(scRNA_data=scRNA_seurat,cluster="integrated_snn_res.0.4")
+# 单独运行
+pbmc.hesc <-SingleR(test pbmc_for_SingleR,ref list(encode,hema,hpca,immune,monaImmune),
+labels list(
+encode$label.main,
+hema$label.main,
+hpca$label.main,
+immune$label.main,
+monaImmune$label.main),clusters=clusters)

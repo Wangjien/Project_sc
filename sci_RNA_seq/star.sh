6da893ef00aaef09cc555a936026ca3cfe3e1c5c
Wangjien <52444578+Wangjien@users.noreply.github.com>
2023-03-11 00:57:25 +0800

    Create star.sh

---
+++ sci_RNA_seq/star.sh
@@ -0,0 +1,12 @@
+thread=20
+genomeDir="/root/wangje/Reference/Star_index/UCSC"
+input_file="/root/wangje/Project/Yin/Data/extract_umi_barcode/I230303_I230303-11_2.barcode.clearn.fastq.gz"
+output_file_prefix="I230303_I230303-11_2.barcode.clearn.star"
+
+STAR --runThreadN $thread \
+--genomeDir $genomeDir \
+--readFilesIn $input_file \
+--readFilesCommand zcat \
+--outFilterMultimapNmax 1 \ #比对时允许的最大错配数
+--outFileNamePrefix $output_file_prefix \
+--outSAMtype BAM SortedByCoordinate

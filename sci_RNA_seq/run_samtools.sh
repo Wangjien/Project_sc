cb1e4161e3ded4ed280cb2c6f414eb731bf09873
Wangjien <52444578+Wangjien@users.noreply.github.com>
2023-03-11 01:42:08 +0800

    Create run_samtools.sh

---
+++ sci_RNA_seq/run_samtools.sh
@@ -0,0 +1,5 @@
+samtools sort -m 15000000000 
+I230303_I230303-11_2.barcode.clearn.starAligned.sortedByCoord.out.bam.featureCounts.bam -o 
+test.assigned_sorted.bam
+
+samtools index test.assigned_sorted.bam

486864b907c3624d78df0a811337539e9ee7c20b
Wangjien <52444578+Wangjien@users.noreply.github.com>
2023-06-25 01:13:29 +0800

    new

--- infercnv/Start_inferCNV.R
+++ infercnv/Start_inferCNV.R
@@ -2,4 +2,8 @@ library(infercnv)
 library(dplyr)
 library(tidyverse)
 library(ggplot2)
-library()
+library(AnnoProbe)
+
+# load data 
+big.fastmnn = '/root/wangje/wangjien_new/liu-big/scRNA_FastMNN_new.RData'
+load(big.fastmnn)

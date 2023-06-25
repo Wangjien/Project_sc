e6d058e74210e04e2aaf1423148a21ed00c60702
Wangjien <52444578+Wangjien@users.noreply.github.com>
2023-06-25 08:24:43 +0800

    new

--- infercnv/Start_inferCNV.R
+++ infercnv/Start_inferCNV.R
@@ -3,7 +3,13 @@ library(dplyr)
 library(tidyverse)
 library(ggplot2)
 library(AnnoProbe)
+library()
 
 # load data 
 big.fastmnn = '/root/wangje/wangjien_new/liu-big/scRNA_FastMNN_new.RData'
 load(big.fastmnn)
+
+# 提取部分文件
+scRNA_sub = scRNA[,scRNA$celltype %in% c('Fibroblasts','Endothelials','Epithelials')]
+# 
+

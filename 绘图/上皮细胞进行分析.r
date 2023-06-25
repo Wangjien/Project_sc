6fc92fff4f114d6fa5ef938a94dbae9abb82af3f
Wangjien <52444578+Wangjien@users.noreply.github.com>
2023-06-25 09:50:37 +0800

    new

--- 绘图/上皮细胞进行分析.r
+++ 绘图/上皮细胞进行分析.r
@@ -6,7 +6,6 @@ library(ggplot2)
 # ----------------------------------------------------------------
 # 使用fastmnn 的大群结果进行分分析
 # ----------------------------------------------------------------
-
 plist = list()
 for(i in names(flist)){
     plist = DimPlot(flist[i], raster = F) + labs(title = i)

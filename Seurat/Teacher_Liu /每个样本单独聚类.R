0f7739dfc92bb9474f5749a865df20c5d7d2d52f
Wangjien <52444578+Wangjien@users.noreply.github.com>
2023-07-05 18:37:42 +0800

    new

--- Seurat/Teacher_Liu /每个样本单独聚类.R
+++ Seurat/Teacher_Liu /每个样本单独聚类.R
@@ -56,3 +56,92 @@ for(i in 1:length(flist)){
         plist2[[names(flist[i])]] = DimPlot(flist[[i]], label=T, repel = T, raster=F)
     })
 }
+
+
+#----------------------------------------------------------------
+# 查看T细胞中的其他亚群marker
+Tcells <- c('CD3D','CD3G','CD2')
+NK_cells <- c('KLRC1','KLRC3','TRDV9')
+Fibroblasts <- c('COL1A1','DCN','LUM')
+Myeloids <- c('LYZ','CD68','TYROBP')
+Epithelial <- c('CD24','KRT19','EPCAM')
+Bcells <- c('CD79A','CD19','MS4A1')
+Endothelial <- c('CLDN5','FLT1','RAMP2')
+Plasma <- c('IGHG1','JCHAIN','MZB1')
+Hepatocytes <- c('ALB','APOB','HP')
+Keratinocytes <- c("KRT5","KRT14","FABP5")
+DC <- c("LILRA4","CXCR3","IRF7")
+Mast <- c("CPA3","TPSABT","TPSB2")
+
+marker.list <- list("NK cell"=NK_cells,"T cell"=Tcells,'B cell'=Bcells,
+                    "Plasmas" =Plasma,Myeloids=Myeloids,Fibroblasts=Fibroblasts,
+                    Epithelials=Epithelial,Endothelials=Endothelial,Hepatocytes=Hepatocytes,
+                    Keratinocytes=Keratinocytes,DC = DC, Mast = Mast)
+                    
+plotFeature <- function(scRNA_data = scRNA_data,
+                        choose = "Feature",
+                        col_num = 6, marker.list = marker.list,...) {
+    pacman::p_load("Seurat", "ggplot2", "tidyverse")
+    DefaultAssay(scRNA_data) <- "RNA"
+    plist <- list()
+    if (is.null(choose)) {
+        message("请选择绘图类型")
+    } else if (choose == "Feature") {
+        for (i in names(marker.list)) {
+            for (j in marker.list[[i]]) {
+                #    print(paste0(i,"_",j))
+                tmp <- tryCatch(
+                    {
+                        FeaturePlot(scRNA_data, features = j,raster=F) +
+                            theme(legend.position = "right") +
+                            labs(title = paste0(i, "_", j))
+                    },
+                    error = function(e) {
+                        message("Error @ ", j)
+                        return(NA)
+                    },
+                    finally = {
+                        message(paste0(i, "_", j, "_next..."))
+                    }
+                )
+                plist[[paste0(i, "_", j)]] <- tmp
+            }
+        }
+        p_new <- Filter(Negate(anyNA), plist)
+        p <- wrap_plots(p_new, bycol = T, ncol = col_num)
+        return(p)
+    } else if (choose == "SCpubr") {
+        for (i in names(marker.list)) {
+            for (j in marker.list[[i]]) {
+                #    print(paste0(i,"_",j))
+                tmp <- tryCatch(
+                    {
+                        SCpubr::do_NebulosaPlot(scRNA_data, features = j, viridis_color_map = "H", pt.size = 0.02) +
+                            theme(legend.position = "righty") +
+                            labs(title = paste0(i, "_", j))
+                    },
+                    error = function(e) {
+                        message("Error @ ", j)
+                        return(NA)
+                    },
+                    finally = {
+                        message(paste0(i, "_", j, "_next..."))
+                    }
+                )
+                plist[[paste0(i, "_", j)]] <- tmp
+            }
+        }
+        p_new <- Filter(Negate(anyNA), plist)
+        p <- wrap_plots(p_new, bycol = T, ncol = col_num)
+        return(p)
+    }
+    ...
+}
+
+# 循环绘图
+plist3 = list()
+for(index1 in 1:length(flist)){
+    flog.info(names(flist[index1]))
+    plist3[[names(flist[index])]] = plotFeature(flist[[index1]], choose = 'Feature', 
+                                        marker.list = marker.list, col_num = 6)
+}
